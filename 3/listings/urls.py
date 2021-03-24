c59de24aa6eb1c95498033e1120db7728182d460
Jeton Stojku <jetonstojku@hotmail.com>
2021-03-25 00:53:50 +0100

    7

--- 3/listings/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index1'),
-    path('about', views.about, name='about2'),
-]
--- 6/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('test_1', views.test_1, name='test_1'),
-]
